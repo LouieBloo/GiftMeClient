2b79d9c36083861922b76241fa469ef916183d8f
domleggio <domleggio@gmail.com>
2020-10-13 22:59:40 -0700

    added email and clickable to clipboard

--- src/app/components/pages/contact/contact.component.ts
+++ src/app/components/pages/contact/contact.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { NgbDate, NgbTooltip } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-contact',
@@ -8,6 +9,32 @@ import { Component, OnInit } from '@angular/core';
 export class ContactComponent implements OnInit {
 
   constructor() { }
+
+  @ViewChild("titleTooltip", { static: false }) titleTooltip: NgbTooltip;
+
+  copyMessage(val: string){
+    const selBox = document.createElement('textarea');
+    selBox.style.position = 'fixed';
+    selBox.style.left = '0';
+    selBox.style.top = '0';
+    selBox.style.opacity = '0';
+    selBox.value = val;
+    document.body.appendChild(selBox);
+    selBox.focus();
+    selBox.select();
+    document.execCommand('copy');
+    document.body.removeChild(selBox);
+
+    this.showTooltip();
+  }
+
+  showTooltip = () => {
+    if (true) {
+      setTimeout(() => {
+        this.titleTooltip.open();
+      }, 200)
+    }
+  }
 
   ngOnInit() {
   }
